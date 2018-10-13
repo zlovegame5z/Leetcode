62e04f7153bbff79bc09e2959c81b0ba4f8d1e0b
NickWu <zlovegame5z@gmail.com>
2018-10-13 13:29:06 +0800

    finish
    d

--- src/JewelsAndStones.java
+++ src/JewelsAndStones.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/*
+/*  2018/10/13
 *
 *
 You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
